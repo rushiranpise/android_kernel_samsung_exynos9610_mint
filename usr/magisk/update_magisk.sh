3f173bf657416a1448fbe3646a951d9dfe464f73
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-06-14 04:56:30 +0000

    buid magisk delta lite

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -9,18 +9,18 @@ ver="$(cat "$DIR/magisk_version" 2>/dev/null || echo -n 'none')"
 if [ "x$1" = "xcanary" ]
 then
 	nver="canary"
-	magisk_link="https://huskydg.github.io/magisk-files/app-release.apk"
+	magisk_link="https://github.com/rushiranpise/magisk-files/raw/main/delta-lite.apk"
 elif [ "x$1" = "xlocal" ]
 then
 	nver="local"
-	magisk_link="https://huskydg.github.io/magisk-files/app-release.apk"
+	magisk_link="https://github.com/rushiranpise/magisk-files/raw/main/delta-lite.apk"
 else
 	if [ "x$1" = "x" ]; then
 		nver="$(curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json | jq '.magisk.version' | cut -d '"' -f 2)"
 	else
 		nver="$1"
 	fi
-	magisk_link="https://huskydg.github.io/magisk-files/app-release.apk"
+	magisk_link="https://github.com/rushiranpise/magisk-files/raw/main/delta-lite.apk"
 fi
 
 if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/arm/magiskinit64" \) -o \( "$nver" = "canary" \) -o \( "$nver" = "local" \) ]
